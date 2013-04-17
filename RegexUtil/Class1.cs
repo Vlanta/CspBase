17340f0f3ec481eeb9c056bdd2ed1ada3995ff9d
CodeRushing <wentleing@gmail.com>
2013-04-17 14:22:01 +0800

    remove empty

--- RegexUtil/Class1.cs
+++ RegexUtil/Class1.cs
@@ -13,17 +13,18 @@ namespace RegexUtil
             //正则表达式的一般用法
             Regex regex = new Regex(@"\w+=\w+");
             Match match = regex.Match("          @@@@123=123=");
+            //查询整个字符串
             if (match.Success)
             {
                 Console.WriteLine(match.Groups[0].Value);
             }
-
-            //int[] groupnums = regex.GetGroupNumbers();     
-            //while(match.Success)
-            //{
-            //    Console.WriteLine(match.Groups[0].Value);
-            //    match = match.NextMatch();
-            //}
+            //查询分组
+            int[] groupnums = regex.GetGroupNumbers();
+            while (match.Success)
+            {
+                Console.WriteLine(match.Groups[0].Value);
+                match = match.NextMatch();
+            }
         }
 
 
